439f1ff0a2ee04d0877c57f3932a1cf5809008bb
rbiradar1 <67091973+rbiradar1@users.noreply.github.com>
2021-08-03 10:27:51 +0530

    Add files via upload

---
+++ greetings/greetings.go
@@ -0,0 +1,14 @@
+package greetings
+
+import (
+	"errors"
+	"fmt"
+)
+
+func Hello(name string) (string, error) {
+	if name == "" {
+		return "", errors.New("Empty Name")
+	}
+	message := fmt.Sprintf("Hi %v! Welcome.", name)
+	return message, nil
+} //Hello is exported name cuz first letter capital
